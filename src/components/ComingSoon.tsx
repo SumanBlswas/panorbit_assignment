724b3f3c61ca572286cd773db48c2a91243d737c
Suman Biswas <sumanmanner@gmail.com>
2023-10-09 04:30:33 +0530

    updated

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,7 +1,13 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 const ComingSoon = () => {
-  return <Box>ComingSoon</Box>;
+  return (
+    <Box>
+      <Text fontSize={"8xl"} fontWeight={"black"} textAlign={"center"} color={"blackAlpha.200"}>
+        Coming Soon
+      </Text>
+    </Box>
+  );
 };
 
 export default ComingSoon;
